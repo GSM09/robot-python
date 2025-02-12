3534d1e2c7c2b347f36427102bf9dc6e43f54934
GSM <koo4927@naver.com>
2025-02-12 12:22:31 +0900

    사용자 함수
    함수 구현

--- 369게임.py
+++ 369게임.py
@@ -4,9 +4,11 @@ import random
 사용하는 input() 함수를 통해 해당 숫자 혹은 3, 6, 9가 들어가는 곳에서는 "짝"을 입력하여야 함
 """
 value = 1
-user = input("해당하는 숫자 입력: ")
+my_turn = True
 
 def 사용자():
+    user = input("해당하는 숫자 입력: ")
+    return user
 
 def 확인():
 
@@ -17,6 +19,9 @@ def 컴퓨터():
 # 무한 반복하게 만들고, 사용자가 틀린 값 입력 시 에러 출력
 while value:
         # 사용자 입력
+    if my_turn:
+        # 사용자 입력
+        user_input = 사용자()
         # 3, 6, 9가 포함되어 있나 확인
         # 다음 차례로 이동
 
